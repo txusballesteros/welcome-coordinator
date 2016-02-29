19fbe1e88fad430db6181dc3f4e2d17cc13721d8
Txus Ballesteros <txus.ballesteros@gmail.com>
2016-02-29 15:50:34 +0100

    Inflating and Configuring Pages Layouts

---
+++ WelcomeCoordinator/src/main/java/com/redbooth/WelcomeCoordinatorPageInflater.java
@@ -0,0 +1,22 @@
+package com.redbooth;
+
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+class WelcomeCoordinatorPageInflater {
+    private final WelcomeCoordinatorLayout view;
+    private final LayoutInflater inflater;
+
+    protected WelcomeCoordinatorPageInflater(@NonNull WelcomeCoordinatorLayout view) {
+        this.view = view;
+        this.inflater = LayoutInflater.from(view.getContext());
+    }
+
+    protected View inflate(@LayoutRes int layoutResourceId) {
+        final ViewGroup pageView = (ViewGroup)inflater.inflate(layoutResourceId, view, false);
+        return pageView;
+    }
+}
