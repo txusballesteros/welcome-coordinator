7d99bf04840b0c2bdfe1b9f2174fe8095287c95e
Narfss <narfss@gmail.com>
2016-03-07 10:21:25 +0100

    Clean code and added set Colors

--- WelcomeCoordinator/src/main/java/com/redbooth/WelcomeCoordinatorLayout.java
+++ WelcomeCoordinator/src/main/java/com/redbooth/WelcomeCoordinatorLayout.java
@@ -2,13 +2,13 @@ package com.redbooth;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Build;
 import android.support.annotation.LayoutRes;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,36 +20,41 @@ import java.util.List;
 
 public class WelcomeCoordinatorLayout extends HorizontalScrollView {
     public static final int WITHOUT_MARGIN = 0;
+    public static final int RADIUS = 20;
+    public static final int RADIUS_MARGIN = 30;
+    public static final int DEF_INDICATOR_UNSELECTED_COLOR = Color.WHITE;
+    public static final int DEF_INDICATOR_SELECTED_COLOR = Color.BLACK;
     private WelcomeCoordinatorTouchController touchController;
     private WelcomeCoordinatorPageInflater pageInflater;
     private FrameLayout mainContentView;
     private List<WelcomePageBehavior> behaviors = new ArrayList<>();
     private OnPageScrollListener onPageScrollListener;
     private int pageSelected = 0;
-    private Paint paintUnselected;
-    private Paint paintSelected;
-    private Paint paintPreSelected;
+    int indicatorColorUnselected = DEF_INDICATOR_UNSELECTED_COLOR;
+    int indicatorColorSelected = DEF_INDICATOR_SELECTED_COLOR;
+    private Paint indicatorPaintUnselected;
+    private Paint indicatorPaintSelected;
 
     public WelcomeCoordinatorLayout(Context context) {
         super(context);
-        initializeView();
+        initializeView(context, null);
     }
 
     public WelcomeCoordinatorLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initializeView();
+        initializeView(context, attrs);
     }
 
     public WelcomeCoordinatorLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initializeView();
+        initializeView(context, attrs);
     }
 
     @SuppressWarnings("unused")
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public WelcomeCoordinatorLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        initializeView();
+        initializeView(context, attrs);
     }
 
     public void addPage(@LayoutRes int... layoutResourceIds) {
@@ -89,23 +94,25 @@ public class WelcomeCoordinatorLayout extends HorizontalScrollView {
         return result;
     }
 
-    private void initializeView() {
+    private void initializeView(Context context, AttributeSet attrs) {
         this.setHorizontalScrollBarEnabled(false);
         this.setOverScrollMode(OVER_SCROLL_NEVER);
         touchController = new WelcomeCoordinatorTouchController(this);
         pageInflater = new WelcomeCoordinatorPageInflater(this);
+        extractAttributes(context, attrs);
         buildMainContentView();
         attachMainContentView();
-        configureCounter();
+        configureIndicatorColors();
     }
 
-    private void configureCounter() {
-        paintUnselected = new Paint();
-        paintUnselected.setColor(Color.WHITE);
-        paintSelected = new Paint();
-        paintSelected.setColor(Color.BLACK);
-        paintPreSelected = new Paint();
-        paintPreSelected.setColor(Color.GRAY);
+    private void extractAttributes(Context context, AttributeSet attrs) {
+        final TypedArray attributes
+                = context.obtainStyledAttributes(attrs, R.styleable.WelcomeCoordinatorLayout);
+        indicatorColorUnselected
+                = attributes.getColor(R.styleable.WelcomeCoordinatorLayout_indicatorUnselected, DEF_INDICATOR_UNSELECTED_COLOR);
+        indicatorColorSelected
+                = attributes.getColor(R.styleable.WelcomeCoordinatorLayout_indicatorSelected, DEF_INDICATOR_SELECTED_COLOR);
+        attributes.recycle();
     }
 
     private void buildMainContentView() {
@@ -120,6 +127,23 @@ public class WelcomeCoordinatorLayout extends HorizontalScrollView {
         removeAllViews();
         setClipChildren(false);
         addView(mainContentView);
+    }
+
+    private void configureIndicatorColors() {
+        indicatorPaintUnselected = new Paint();
+        indicatorPaintUnselected.setColor(indicatorColorUnselected);
+        indicatorPaintSelected = new Paint();
+        indicatorPaintSelected.setColor(indicatorColorSelected);
+    }
+
+    public void setIndicatorColorSelected(int indicatorColorSelected) {
+        this.indicatorColorSelected = indicatorColorSelected;
+        configureIndicatorColors();
+    }
+
+    public void setIndicatorColorUnselected(int indicatorColorUnselected) {
+        this.indicatorColorUnselected = indicatorColorUnselected;
+        configureIndicatorColors();
     }
 
     @Override
@@ -149,55 +173,19 @@ public class WelcomeCoordinatorLayout extends HorizontalScrollView {
     }
 
     private void drawIndicator(Canvas canvas) {
-        int radius = 20;
-        int margin = 30;
-        int centerX = (getWidth() - radius)/2 + radius/2;
-        int indicatorWidth = radius * 2;
-        int indicatorAndMargin = indicatorWidth + margin;
+        int centerX = (getWidth() - RADIUS)/2 + RADIUS/2;
+        int indicatorWidth = RADIUS * 2;
+        int indicatorAndMargin = indicatorWidth + RADIUS_MARGIN;
         int leftIndicators = centerX - ((getNumOfPages()-1) * indicatorAndMargin) / 2 ;
-        int positionY = getHeight() - radius - margin;
-
-        //all the points
+        int positionY = getHeight() - RADIUS - RADIUS_MARGIN;
         for (int i = 0; i < getNumOfPages(); i++) {
             int x = leftIndicators + indicatorAndMargin * i + getScrollX();
-            canvas.drawCircle(x, positionY, radius, paintUnselected);
+            canvas.drawCircle(x, positionY, RADIUS, indicatorPaintUnselected);
         }
-
-        //Previous page
-        float preSelectedXPosition = leftIndicators + getScrollX() + getPageSelected() * indicatorAndMargin;
-        //canvas.drawCircle(preSelectedXPosition, positionY, radius, paintPreSelected);
-
-        //Selected page with movement
         float width = (float) getWidth();
         float scrollProgress = getScrollX() / width;
         float selectedXPosition = leftIndicators + getScrollX() + scrollProgress * indicatorAndMargin;
-        //canvas.drawCircle(selectedXPosition, positionY, radius, paintSelected);
-
-        int top = getHeight() - indicatorAndMargin;
-        int bottom = top + radius*2;
-        /*Rectangle
-        if (preSelectedXPosition <= selectedXPosition) {
-            canvas.drawRoundRect(preSelectedXPosition - radius, top, selectedXPosition + radius, bottom, radius, radius, paintPreSelected);
-        } else {
-            canvas.drawRoundRect(selectedXPosition - radius, top, preSelectedXPosition + radius, bottom, radius, radius, paintPreSelected);
-        }
-        */
-
-        if (preSelectedXPosition <= selectedXPosition) {
-            int halfWide = radius * 4 / 2;
-            float f2 = selectedXPosition - preSelectedXPosition;
-            Log.d("DIFERENCE", "diference:" + f2);
-            float leftSide = Math.min(halfWide, f2);
-            float rightSide = Math.min(0, preSelectedXPosition - radius - selectedXPosition + radius);
-            canvas.drawRoundRect(selectedXPosition - radius, top, selectedXPosition + radius, bottom, radius, radius, paintPreSelected);
-        } else {
-            canvas.drawRoundRect(selectedXPosition - radius, top, selectedXPosition + radius, bottom, radius, radius, paintPreSelected);
-        }
-
-    }
-
-    public int getPageSelected() {
-        return pageSelected;
+        canvas.drawCircle(selectedXPosition, positionY, RADIUS, indicatorPaintSelected);
     }
 
     @Override
